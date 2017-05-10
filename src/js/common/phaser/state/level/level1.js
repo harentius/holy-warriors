27254ea675905679bd79da3ca672daa493021459
Yaroslav Osypenko <harentius@yandex.ru>
2017-05-10 22:40:51 +0300

    added stranger and smoking animation

--- src/js/common/phaser/state/level/level1.js
+++ src/js/common/phaser/state/level/level1.js
@@ -12,12 +12,18 @@ export default {
             },
             'spritesheet': {
                 'developer-behind-desk': ['assets/img/level1/developer-behind-desk.png', 40, 29, 8],
+                'stranger': ['assets/img/level1/stranger.png', 16, 35, 8],
             }
         }, phaserGame, window.hw.assetVersion);
     },
     create: () => {
         phaserGame.add.sprite(0, 0, 'office');
         phaserGame.add.sprite(0, config.height - 49 - 2, 'door');
+
+        let stranger = phaserGame.add.sprite(10, config.height - 35 - 2, 'stranger');
+        stranger.animations.add('smoking');
+        stranger.animations.play('smoking', 8, true);
+
         let developerBehindDesk = phaserGame.add.sprite(50, config.height - 29 - 2, 'developer-behind-desk');
         developerBehindDesk.animations.add('typing');
         developerBehindDesk.animations.play('typing', 8, true);
